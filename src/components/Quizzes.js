ad67b6e9119a64bce769eeb8fef62a136a9c4c24
Max Pinheiro <maxpinheiro181@gmail.com>
2021-05-13 16:46:06 -0400

    quiz styling

--- src/components/Quizzes.js
+++ src/components/Quizzes.js
@@ -2,37 +2,48 @@ import React, { useState } from 'react';
 import { proxy } from 'valtio'
 import quizzes from '../media/quizzes.json';
 import Error404 from "./404";
+import bird from '../media/bird.png';
 
 const colors = ['bg-orange', 'bg-yellow', 'bg-green'];
 const state = proxy({colorIdx: Math.floor(Math.random() * colors.length)});
 
 const Quiz = ({gender, addScore}) => {
-    const [question, setQuestion] = useState(0);
+    const [question, setQuestion] = useState(-1);
     const [score, setScore] = useState(0);
     const color = () => {const c = colors[state.colorIdx]; state.colorIdx = (state.colorIdx + 1) % colors.length; return c;};
 
     return (
         <div className="calibri">
-            { question < quizzes[gender]["questions"].length ?
+            <div className="py-3"/>
+            {question === -1 ?
                 <div>
-                    <p className="text-2rem">Question {question + 1}: {quizzes[gender]["questions"][question]["question"]}</p>
-                    { quizzes[gender]["questions"][question]["options"].map(option => (
-                        <div key={option}>
-                            <button className={`multiple-choice-btn ${color()}`}
-                                onClick={() => {
-                                setQuestion(question + 1);
-                                setScore(score + (option === quizzes[gender]["questions"][question]["answer"] ? 1 : 0))
-                            }}>{option}</button>
-                        </div>
-                    )) }
+                    <img src={bird} alt="bird" className="w-40vw mx-auto img-bw"/>
+                    <p className="text-2rem">{gender === 'men' ? 'Bachelor\'s' : 'Bachelorette\'s'} Quest</p>
+                    <p>(Whosoever shall score the highest on this trivia trial will be granted the distinction of serving as {gender === 'men' ? 'Scott\'s Best Man' : 'Lisa\'s Maid of Honor'} today. Good luck.)</p>
+                    <button className={`btn text-2rem ${color()}`} onClick={() => {setScore(0); setQuestion(0);}}>Start</button>
                 </div> :
-                <div className="text-2rem">
-                    <div className="h-20vh" />
-                    <p>Your score: {score}</p>
-                    <button className={`btn text-2rem ${color()}`} onClick={() => {setScore(0); setQuestion(0);}}>Retake Quiz</button>
+                <div>
+                    { question < quizzes[gender]["questions"].length ?
+                        <div>
+                            <p className="text-2rem">Question {question + 1}: {quizzes[gender]["questions"][question]["question"]}</p>
+                            { quizzes[gender]["questions"][question]["options"].map(option => (
+                                <div key={option}>
+                                    <button className={`multiple-choice-btn ${color()}`}
+                                            onClick={() => {
+                                                setQuestion(question + 1);
+                                                setScore(score + (option === quizzes[gender]["questions"][question]["answer"] ? 1 : 0))
+                                            }}>{option}</button>
+                                </div>
+                            )) }
+                        </div> :
+                        <div className="text-2rem">
+                            <div className="h-20vh" />
+                            <p>Your score: {score}</p>
+                            <button className={`btn text-2rem ${color()}`} onClick={() => {setScore(0); setQuestion(0);}}>Retake Quiz</button>
+                        </div>
+                    }
                 </div>
             }
-
         </div>
     )
 }
