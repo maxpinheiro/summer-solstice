ea8f3b2f1d5a9ca4810fd34a1dc6fccbb2f2d8c8
Max Pinheiro <maxpinheiro181@gmail.com>
2021-06-07 23:17:37 -0400

    remove memories tag

--- src/components/eggs/Memories.js
+++ src/components/eggs/Memories.js
@@ -5,7 +5,6 @@ import veterans from "../../media/veterans.mp4";
 const Memories = () => {
     return (
         <div className="container calibri mt-3">
-            <h1 className="text-center">Memories</h1>
             <video width="100%" height="100%" controls poster={veteransCover}>
                 <source src={veterans} type="video/mp4" />
                 Your browser does not support the video tag.
