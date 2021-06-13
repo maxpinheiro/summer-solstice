2fd64f171db76c9ebf4cf239b490b90cf85827c0
Max Pinheiro <maxpinheiro181@gmail.com>
2021-06-13 13:16:55 -0400

    upload newbies video

--- src/components/eggs/Virgin.js
+++ src/components/eggs/Virgin.js
@@ -1,11 +1,16 @@
 import React, {useEffect} from 'react';
+import solsticeNewbiesCover from "../../media/solsticeNewbiesCover.png";
+import solsticeNewbies from "../../media/solsticeNewbies.mp4";
 
 const Virgin = () => {
     useEffect(() => {document.title = 'Virgin | Solstice 2021'});
 
     return (
         <div className="container calibri mt-3">
-            <h1 className="text-center">Virgin</h1>
+            <video width="100%" height="100%" controls poster={solsticeNewbiesCover}>
+                <source src={solsticeNewbies} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
         </div>
     )
 }
